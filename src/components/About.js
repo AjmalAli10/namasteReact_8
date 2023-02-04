6c429f43f72121fd68e20261f7bdd88cf57b80ba
Ajmal Ali <officialajmalali10@gmail.com>
2023-02-04 12:55:17 +0530

    added namasteReact_8 folders and file

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import "../styles/About.css";
+const About = ()=>{
+    return(
+        <div className="about">
+        <h1>About me Page</h1>
+        <h2>This is the Namaste React course 07 , Finding the Path 🚀</h2>
+        </div>
+    )
+};
+export default About;
